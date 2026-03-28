a7948219e1de8a1fdff6c2c81fa3c7229d91d440
Didier Estiben Piracoca Campos <98455168+diperpiracoca@users.noreply.github.com>
2026-03-28 20:29:58 +0100

    Bajada de version para compatibilidad de firebase

--- devhelper-web/src/app/app.spec.ts
+++ devhelper-web/src/app/app.spec.ts
@@ -16,7 +16,7 @@ describe('App', () => {
 
   it('should render title', async () => {
     const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
+    fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, devhelper-web');
   });
