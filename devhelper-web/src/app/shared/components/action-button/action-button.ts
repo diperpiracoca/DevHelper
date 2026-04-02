343f428993be0db836f638b59f801eb3917db01c
Didier Estiben Piracoca Campos <98455168+diperpiracoca@users.noreply.github.com>
2026-04-02 14:09:45 +0200

    Cambios en home

--- devhelper-web/src/app/shared/components/action-button/action-button.ts
+++ devhelper-web/src/app/shared/components/action-button/action-button.ts
@@ -8,6 +8,9 @@ import { Component, input } from '@angular/core';
 })
 export class ActionButton {
   icon = input<string>();
+  styleClass = input<string>();
+  iconClass = input<string>();
   type = input<'circle' | 'square'>('square');
+  size = input<'xs' | 'sm' | 'md' | 'lg' | 'xl'>('md')
   severity = input<'primary' | 'secundary' | 'light'>('primary');
 }
